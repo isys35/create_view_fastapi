c2b647936eba2457c09faeb79a19f8244fd884d1
isys35 <isys35@mail.ru>
2021-10-07 16:13:07 +0300

    set metadata in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,24 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from models import Base
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = Base
 
 
 def run_migrations_offline():
