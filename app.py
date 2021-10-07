4bed3ad83916ab0891570dc3ad4bf06a77370866
isys35 <isys35@mail.ru>
2021-10-07 17:25:42 +0300

    add create command endpoint

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ def get_db():
 
 
 @app.get("/commands/", response_model=List[schemas.BotCommand])
-def commands(db: Session = Depends(get_db)):
+def get_commands(db: Session = Depends(get_db)):
     commands = manager.get_commands(db)
     return commands
+
+
+@app.post("/commands/", response_model=schemas.BotCommand)
+def create_command(command: schemas.BotCommand, db: Session = Depends(get_db)):
+    db_command = manager.create_command(db, command)
+    return db_command
--- db/manager.py
+++ db/manager.py
@@ -1,6 +1,14 @@
-from db.core import Session
+import schemas
+from sqlalchemy.orm import Session
 from db import models
 
 
 def get_commands(db: Session):
     return db.query(models.BotCommand).all()
+
+
+def create_command(db: Session, command: schemas.BotCommand):
+    db_command = models.BotCommand(value=command.value)
+    db.add(db_command)
+    db.commit()
+    return db_command
