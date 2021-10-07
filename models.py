f8663b9cd62925e120c035869af78e4e8a663d47
isys35 <isys35@mail.ru>
2021-10-07 15:54:52 +0300

    removed unnecessary

--- models.py
+++ models.py
@@ -1,10 +1,6 @@
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.dialects.postgresql import VARCHAR, DOUBLE_PRECISION
 
 from sqlalchemy.ext.declarative import declarative_base
-from db import Session
-
-from db import engine
 
 Base = declarative_base()
 
@@ -14,9 +10,3 @@ class BotCommand(Base):
 
     id = Column(Integer, primary_key=True)
     value = Column(String(100), unique=True)
-
-
-bot_command = BotCommand(value='/start')
-with Session() as session:
-    session.add(bot_command)
-    session.commit()
