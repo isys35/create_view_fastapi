046f47210e6737fffab404d5355be8780e92576a
isys35 <isys35@mail.ru>
2021-10-07 16:10:21 +0300

    add dotenv set settings alembic.ini

--- db.py
+++ db.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-POSTGRES_DB = 'postgres'
-POSTGRES_USER = 'postgres'
-POSTGRES_PASSWORD = 'postgres'
+load_dotenv()
+
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DB_URI = 'postgresql://{}:{}@localhost:5432/{}'.format(POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB)
 
 engine = create_engine(DB_URI, echo=True)
