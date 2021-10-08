3a8e79b6f826a187016f314cb966d01cc8ff359f
isys35 <isys35@mail.ru>
2021-10-08 13:53:09 +0300

    remake create command

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import FastAPI, Depends
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
 
 from db import manager
 from db.core import Session
@@ -25,5 +26,12 @@ def get_commands(db: Session = Depends(get_db)):
 
 @app.post("/commands/", response_model=schemas.BotCommand)
 def create_command(command: schemas.BotCommand, db: Session = Depends(get_db)):
+    db_command = manager.get_command(db, value=command.value)
+    if db_command:
+        raise HTTPException(status_code=400, detail="Данная комманда уже есть")
     db_command = manager.create_command(db, command)
     return db_command
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- db/manager.py
+++ db/manager.py
@@ -11,4 +11,9 @@ def create_command(db: Session, command: schemas.BotCommand):
     db_command = models.BotCommand(value=command.value)
     db.add(db_command)
     db.commit()
+    db.refresh(db_command)
     return db_command
+
+
+def get_command(db: Session, value: str):
+    return db.query(models.BotCommand).filter(models.BotCommand.value == value).first()
